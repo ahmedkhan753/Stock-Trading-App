72a232a2916d0155217e9bdbbd98566c606b01bf
ahmedkhan753 <ahmedk32410@gmail.com>
2025-10-15 22:43:59 +0500

    Defined project structure and created Java models.

--- src/models/Stock.java
+++ src/models/Stock.java
@@ -2,4 +2,43 @@ package models;
 
 public class Stock {
     
+    // Attributes for the stock
+    private String symbol;
+    private String name;
+    private double price;
+    private int id;
+
+    // Constructor for the stock
+    public Stock(String symbol, String name, double price, int id) {
+        this.symbol = symbol;
+        this.name = name;
+        this.price = price;
+        this.id = id;
+    }
+
+    // Getters and Setters
+    public String getSymbol() {
+        return symbol;
+    }
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/models/Transaction.java
+++ src/models/Transaction.java
@@ -1,5 +1,77 @@
 package models;
+import java.sql.Timestamp;
 
 public class Transaction {
+    private int id;
+    private int userID;
+    private int stockID;
+    private int quantity;
+    private double price;         //The price at the time of transaction
+    private Timestamp timestamp;
+    private String type;          //buy or sell
+
+    // Constructor for the transaction ( When reading from the database)
+    public Transaction(int id, int userID, int stockID, int quantity, double price
+            , Timestamp timestamp, String type) {
+        this.id = id;
+        this.userID = userID;
+        this.stockID = stockID;
+        this.quantity = quantity;
+        this.price = price;
+        this.timestamp = timestamp;
+        this.type = type;
+    }
+
+    // Constructor for the transaction ( When creating a new transaction)
+    public Transaction(int userID, int stockID, int quantity, double price, String type) {
+        this.userID = userID;
+        this.stockID = stockID;
+        this.quantity = quantity;
+        this.price = price;
+        this.type = type;
+    }
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getUserID() {
+        return userID;
+    }
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+    public int getStockID() {
+        return stockID;
+    }
+    public void setStockID(int stockID) {
+        this.stockID = stockID;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
     
 }
